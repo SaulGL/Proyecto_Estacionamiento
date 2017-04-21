017a1c625cdea2ad5530920a8e8bc1c7409ccd2e
saul <saul@169.254.231.143>
2017-04-20 23:48:06 -0500

    Colocacion de Hola a todos y adios

--- src/estacionamiento/Estacionamiento.java
+++ src/estacionamiento/Estacionamiento.java
@@ -8,7 +8,7 @@ public class Estacionamiento {
         Inicio i1;
         i1 = new Inicio();
         i1.init(); 
-        System.out.println("Hola Mundo a todos ");
+        System.out.println("Hola Mundo a todos  y adios");
         /*
         Cliente i1;
         i1 = new Cliente();
